f313807ece60dc6b7d22dbc1b3b11d68af8989d4
Samuel Barona <100160694+Samub96@users.noreply.github.com>
2023-09-13 09:57:34 -0500

    proyecto borrador #1

---
+++ inferencia R/Inferencia_proyecto.R
@@ -0,0 +1,13 @@
+library(readr)
+
+
+#directorio de trabajo
+setwd('C:/Users/Lina Andrade/Desktop/inferencia R')
+
+#directorio en el que se est? trabajando
+getwd()
+
+#Leer la base de datos
+
+Base_datos <- read_csv("All Countries.csv")
+
