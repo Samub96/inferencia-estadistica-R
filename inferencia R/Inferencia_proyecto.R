ccb96b2ee4039396fdc3ec2761587c90d4f20a13
Samub96 <samuel.d.barona@gmail.com>
2023-09-23 11:04:43 -0500

    plantilla de markDown

--- inferencia R/Inferencia_proyecto.R
+++ inferencia R/Inferencia_proyecto.R
@@ -2,12 +2,17 @@ library(readr)
 
 
 #directorio de trabajo
-setwd('C:/Users/Lina Andrade/Desktop/inferencia R')
+setwd('C:/Users/Admin/Desktop/R/inferencia-estadistica-R/inferencia R')
 
-#directorio en el que se est? trabajando
+#directorio en el que se esta trabajando
 getwd()
 
 #Leer la base de datos
 
-Base_datos <- read_csv("All Countries.csv")
+Base_datos <- read_csv("cars_dataset.csv")
 
+#exploracion de datos 
+
+summary(Base_datos)
+
+attach(Base_datos)
